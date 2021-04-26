6171c81050946a8d7ea5e87f1ce477b51875b887
FeeeeK <krmrclemens@gmail.com>
2021-04-27 01:02:03 +0300

    some fixes

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
+import os
+import sys
+
 from vkbottle.bot import Bot, Message
 
 from lib import JsonConfig
 from middlewares import FirstMessageMiddleware
 from utils import get_image_url, search
 
-config = JsonConfig("./config.json")
+config = JsonConfig(os.path.realpath(f"{sys.path[0]}/config.json"))
 bot = Bot(config.token, task_each_event=True)
 bot.labeler.message_view.register_middleware(FirstMessageMiddleware())
 
 
-@bot.on.message(regexp=r".*(?i)(anime|аниме).*")
-async def handler(ans: Message, _) -> str:
+@bot.on.message(regexp=r".*(?i)(?:anime|аниме).*")
+async def handler(ans: Message, match) -> str:
     url = get_image_url(ans)
     if not url:
         return "Не удалось найти картинку для поиска в Вашем сообщении!"
--- utils/search.py
+++ utils/search.py
@@ -7,17 +7,19 @@ tracemoe = ATraceMoe()
 
 async def search(url) -> str:
     data = await tracemoe.search(url, is_url=True)
-    title = data["result"][0]["anilist"]["title"]["english"]
-    episode = data["result"][0]["episode"]
-    start = datetime.timedelta(seconds=round(data["result"][0]["from"]))
-    end = datetime.timedelta(seconds=round(data["result"][0]["to"]))
-    similarity = round(data["result"][0]["similarity"] * 100)
+    title = (
+        data["docs"][0]["title_english"]
+        or data["docs"][0]["title"]
+        or data["docs"][0]["synonyms"][0]
+    )
+    episode = data["docs"][0]["episode"] or "1"
+    timecode = datetime.timedelta(seconds=data["docs"][0]["at"] or 0)
+    similarity = round(data["docs"][0]["similarity"] * 100)
 
-    text = "Аниме: {}\nЭпизод: {}\nТаймкод: с {} по {}\nТочность: {}%".format(
+    text = "Аниме: {}\nЭпизод: {}\nТаймкод: {}\nТочность: {}%".format(
         title,
         episode if isinstance(episode, str) else ", ".join(map(str, episode)),
-        start,
-        end,
+        timecode,
         similarity,
     )
     return text
