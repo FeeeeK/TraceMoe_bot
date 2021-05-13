9dfff0c1f27ecd9e8a28e6a4dafb6165b4bdc542
FeeeeK <krmrclemens@gmail.com>
2021-05-13 18:45:58 +0300

    Small changes for the new api (#1)

--- utils/search.py
+++ utils/search.py
@@ -8,13 +8,13 @@ tracemoe = ATraceMoe()
 async def search(url) -> str:
     data = await tracemoe.search(url, is_url=True)
     title = (
-        data["docs"][0]["title_english"]
-        or data["docs"][0]["title"]
-        or data["docs"][0]["synonyms"][0]
+        data["result"][0]["anilist"]["title"]["english"]
+        or data["result"][0]["anilist"]["title"]["romanji"]
+        or data["result"][0]["anilist"]["synonyms"][0]
     )
-    episode = data["docs"][0]["episode"] or "1"
-    timecode = datetime.timedelta(seconds=data["docs"][0]["at"] or 0)
-    similarity = round(data["docs"][0]["similarity"] * 100)
+    episode = str(data["result"][0]["episode"]) or "1"
+    timecode = datetime.timedelta(seconds=data["result"][0]["from"] or 0)
+    similarity = round(data["result"][0]["similarity"] * 100)
 
     text = "Аниме: {}\nЭпизод: {}\nТаймкод: {}\nТочность: {}%".format(
         title,
